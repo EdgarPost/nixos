f1029da1386acc0cb8c465943212e1e30b5236e3
Edgar Post-Buijs <github@edgarpost.com>
2025-12-18 21:48:12 +0100

    fix: disable 1Password GUI until Hyprland is set up

--- modules/nixos/1password.nix
+++ modules/nixos/1password.nix
@@ -1,18 +1,18 @@
-{ config, pkgs, user, ... }:
+{ config, pkgs, lib, user, ... }:
 
 {
-  # 1Password GUI and CLI
+  # 1Password CLI (always available)
   programs._1password.enable = true;
-  programs._1password-gui = {
-    enable = true;
-    polkitPolicyOwners = [ user.name ];
-  };
 
-  # 1Password SSH Agent
-  # This lets 1Password manage your SSH keys
-  # Keys are stored in 1Password, never on disk
-  programs.ssh.extraConfig = ''
-    Host *
-      IdentityAgent ~/.1password/agent.sock
-  '';
+  # 1Password GUI (requires display - enable after Hyprland setup)
+  # programs._1password-gui = {
+  #   enable = true;
+  #   polkitPolicyOwners = [ user.name ];
+  # };
+
+  # 1Password SSH Agent (enable after GUI is set up)
+  # programs.ssh.extraConfig = ''
+  #   Host *
+  #     IdentityAgent ~/.1password/agent.sock
+  # '';
 }
