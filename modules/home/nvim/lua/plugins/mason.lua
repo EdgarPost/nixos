c9bbccbeba182e1b6017c9fb39e6e02d94173e66
Edgar Post-Buijs <github@edgarpost.com>
2026-02-18 20:36:18 +0100

    chore(nvim): update mason plugin org from williamboman to mason-org

--- modules/home/nvim/lua/plugins/mason.lua
+++ modules/home/nvim/lua/plugins/mason.lua
@@ -1,5 +1,5 @@
 -- Disable Mason (we use Nix for LSPs/formatters)
 return {
-  { "williamboman/mason.nvim", enabled = false },
-  { "williamboman/mason-lspconfig.nvim", enabled = false },
+  { "mason-org/mason.nvim", enabled = false },
+  { "mason-org/mason-lspconfig.nvim", enabled = false },
 }
