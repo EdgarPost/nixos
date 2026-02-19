f929f40d7bba5d312317a1feee4b16cab306d080
Edgar Post-Buijs <github@edgarpost.com>
2026-02-19 09:23:44 +0100

    feat(nvim): include hidden files in picker search

--- modules/home/nvim/lua/plugins/ui.lua
+++ modules/home/nvim/lua/plugins/ui.lua
@@ -8,11 +8,21 @@ return {
 	-- Disable all animations (mini.animate)
 	{ "nvim-mini/mini.animate", enabled = false },
 
-	-- Disable snacks.nvim scroll animation
+	-- Disable snacks.nvim scroll animation + show hidden files in picker
 	{
 		"folke/snacks.nvim",
 		opts = {
 			scroll = { enabled = false },
+			picker = {
+				sources = {
+					files = {
+						hidden = true,
+					},
+					grep = {
+						hidden = true,
+					},
+				},
+			},
 		},
 	},
 }
