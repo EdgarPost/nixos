c52877ceefe15a315ac1d010dc4e6ba3c580108c
Edgar Post-Buijs <github@edgarpost.com>
2026-02-19 17:00:21 +0100

    feat(nvim): add GitHub issues and PRs picker via snacks.nvim

--- modules/home/nvim/lua/plugins/ui.lua
+++ modules/home/nvim/lua/plugins/ui.lua
@@ -11,7 +11,14 @@ return {
 	-- snacks.nvim overrides
 	{
 		"folke/snacks.nvim",
+		keys = {
+			{ "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues" },
+			{ "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "GitHub Issues (all)" },
+			{ "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "GitHub PRs" },
+			{ "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "GitHub PRs (all)" },
+		},
 		opts = function(_, opts)
+			opts.gh = {}
 			opts.scroll = { enabled = false }
 			opts.picker = {
 				sources = {
